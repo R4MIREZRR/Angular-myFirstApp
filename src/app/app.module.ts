ad35ee385e24f2748c3d1f957d67fbb450b9299a
Rodrigo <r4mirez.r@gmail.com>
2020-05-17 15:35:00 -0500

    Primera subida

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+
+import { DataService } from './data.service';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Route } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { IndexComponent } from './index/index.component';
+
+const routes: Route[] = [
+  {path: 'home', component: IndexComponent },
+  {path: 'user', component: UserComponent},
+  {path: 'about', component: AboutComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    IndexComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+    
+  ],
+  providers: [DataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
